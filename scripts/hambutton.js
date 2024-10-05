0aff6bad07cefc495309eb340a73c7c3978f4096
siamtx <siamtx@yahoo.com>
2024-10-05 10:29:26 -0400

    removed whitespace in JS files.

--- scripts/hambutton.js
+++ scripts/hambutton.js
@@ -1,6 +1,7 @@
+// Tell the document to look for the hamburger
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
-
+// Tell JS to listen for a ubtton click from a mouse.
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
