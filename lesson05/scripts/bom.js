a223578b295f2763deabc2e65229c5f1209c7472
siamtx <siamtx@yahoo.com>
2024-10-16 20:52:06 -0400

    added lesson05;bom.html, base.css, bom.js

---
+++ lesson05/scripts/bom.js
@@ -0,0 +1,10 @@
+// JavaScript step 1 //
+const input = document.querySelector('favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('list');
+
+// JavaScript step 2 //
+button.addEventListener('click', function() {});
+button.addEventListener('click' () => {});
+
+// JavaScript step 3 //
