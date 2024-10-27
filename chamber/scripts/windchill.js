9a77a68beb04de6653a83afad4db869ce47c7b51
siamtx <siamtx@yahoo.com>
2024-10-26 21:50:59 -0400

    added temp and wind speed. created

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,7 @@
+// Get the Temperature //
+const Temperature = new temp;
+// Get the windspeed //
+const windspeed = new speed;
+//   //
+document.getElementById('Temperature').textContent = `${Temperature}`;
+document.getElementById('Windspeed').textContent = `${Temperature}`;
