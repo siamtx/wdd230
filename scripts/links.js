d2330596cbbd209a54a34332ad538d930c59739e
siamtx <siamtx@yahoo.com>
2024-11-13 22:29:18 -0500

    lesson08 and lesson09 links.json

---
+++ scripts/links.js
@@ -0,0 +1,8 @@
+// Base URL for GitHub repository
+const baseURL = "https://github.com/siamtx/wdd230";
+
+// Reference to links.json file
+const linksURL = "https://github.com/siamtx/wdd230/data/links.json";
+
+
+
