964d84867f83ee21c67ad0280978ceb44072598f
siamtx <siamtx@yahoo.com>
2024-11-13 22:39:16 -0500

    updated the link.js file.  Need to develop the displayLinks().

--- scripts/links.js
+++ scripts/links.js
@@ -4,5 +4,15 @@ const baseURL = "https://github.com/siamtx/wdd230";
 // Reference to links.json file
 const linksURL = "https://github.com/siamtx/wdd230/data/links.json";
 
+// Asynchronous function to fetch the data from links.json
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data);
+    displayLinks(data);
 
+}
 
+getLinks();
+
+// 
